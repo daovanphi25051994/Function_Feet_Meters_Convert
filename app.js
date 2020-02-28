8f8e5b86893eeb2d4d634ddf684f83780d223f5f
DaoVanPhi <daovanphi25051994@gmail.com>
2020-02-28 11:58:47 +0700

    feet meters convert

---
+++ app.js
@@ -0,0 +1,15 @@
+function  feetToMeter(feet) {
+    return 0.305 * feet;
+}
+
+function  meterToFeet(meter) {
+    return 3.279 * meter;
+}
+
+let a = feetToMeter(20);
+console.log(a);
+let b = meterToFeet(a);
+console.log(b)
+let c = feetToMeter(b);
+console.log(c)
+
